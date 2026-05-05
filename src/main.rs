4e0cf9138dfd74adbad786e08da4b65b96579e11
Matt-DESTROYER <matthewsalgorithm@gmail.com>
2026-05-05 14:38:17 +1000

    Basic tokenisation mostly working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use std::fs;
+
 mod args;
 mod tokeniser;
 mod opcodes;
 
 fn main() {
-    let cli_args = args::get_cli_arguments();
+	let cli_args = args::get_cli_arguments();
 
-    println!("{:#?}", cli_args);
+	if cli_args.check_flag("--wat2wasm") {
+		if let Some(file) = cli_args.get_unnamed().last() {
+			let code = match fs::read_to_string(file) {
+				Ok(code) => code,
+				Err(err) => return eprintln!("Error: {}", err)
+			};
+
+			let tokens = tokeniser::tokenise(&code);
+			for token in tokens {
+				println!("{:?}", token);
+			}
+		}
+	}
 }
--- src/tokeniser.rs
+++ src/tokeniser.rs
@@ -1,6 +1,4 @@
-use std::os::raw;
-
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum TokenType {
 	None,
 	Instruction,
@@ -10,6 +8,7 @@ pub enum TokenType {
 	Group,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Token {
 	None,
 	Instruction(String),
@@ -20,15 +19,15 @@ pub enum Token {
 }
 
 #[allow(non_camel_case_types)]
+#[derive(Debug, PartialEq)]
 pub enum Literal {
-	i32(String),
-	i64(String),
-	u32(String),
-	u64(String)
+	Integer(String),
+	Float(String),
+	String(String)
 }
 
 fn token_type(char: char) -> TokenType {
-	if char.is_whitespace() {
+	if char == ',' {
 		TokenType::Separator
 	} else if char == '$' {
 		TokenType::Identifier
@@ -43,7 +42,7 @@ fn token_type(char: char) -> TokenType {
 	}
 }
 
-fn parse_token(raw_token: &str, token_type: TokenType) -> Token {
+pub fn parse_token(raw_token: &str, token_type: TokenType) -> Token {
 	if raw_token.is_empty() {
 		return Token::None
 	}
@@ -51,14 +50,26 @@ fn parse_token(raw_token: &str, token_type: TokenType) -> Token {
 	match token_type {
 		TokenType::Instruction => Token::Instruction(raw_token.to_owned()),
 		TokenType::Identifier => Token::Identifier(raw_token.to_owned()),
-		TokenType::Literal => todo!("Need to look backwards to determine literal type..."),
+		TokenType::Literal => if raw_token.chars().nth(0) == Some('"') &&
+				raw_token.chars().nth_back(0) == Some('"') {
+			let mut chars = raw_token.chars();
+			chars.next();
+			chars.next_back();
+			Token::Literal(Literal::String(chars.as_str().to_owned()))
+		} else if raw_token.contains('.') {
+			Token::Literal(Literal::Float(raw_token.to_owned()))
+		} else {
+			Token::Literal(Literal::Integer(raw_token.to_owned()))
+		},
 		TokenType::Separator => Token::Separator(raw_token.chars().nth(0).unwrap_or('\0')),
 		TokenType::Group => Token::Group(raw_token.chars().nth(0).unwrap_or('\0')),
 		_ => Token::None
 	}
 }
 
-fn push_token(tokens: &mut Vec<Token>, raw_token: &str, token_type: TokenType) {}
+fn push_token(tokens: &mut Vec<Token>, raw_token: &str, token_type: TokenType) {
+	tokens.push(parse_token(raw_token, token_type));
+}
 
 pub fn tokenise(buffer: &str) -> Vec<Token> {
 	let mut tokens = Vec::new();
@@ -82,7 +93,8 @@ pub fn tokenise(buffer: &str) -> Vec<Token> {
 						char == '"' || char == '\'' ||
 						char == ',' {
 					push_token(&mut tokens, &accumulator, accumulator_type);
-					accumulator_type = TokenType::None;
+					accumulator_type = token_type(char);
+					accumulator = String::from(char);
 				} else {
 					accumulator.push(char);
 				}
